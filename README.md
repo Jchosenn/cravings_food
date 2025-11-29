64bfa4c9fa9be74671d83b8d1941a14b690f77e5
Jchosenn <jonathanchosen79@gmail.com>
2025-11-29 17:08:52 +0100

    chore: project setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# cravings_food
-
+# Cravings
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='bg-black'>
+        <h1 className='text-orange-400 text-3xl'>hello world</h1>
+    </div>
+  )
+}
+
+export default App
